49c38ec5f885c86cf353c5eaff8c9802c4a4bd65
Cjsah <2684117397@qq.com>
2023-06-20 14:03:24 +0800

    init

---
+++ src/main/java/net/cjsah/update/UpdateSuppression.java
@@ -0,0 +1,17 @@
+package net.cjsah.update;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.math.BlockPos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UpdateSuppression implements ModInitializer {
+    private static final List<BlockPos> suppressionBlocks = new ArrayList<>();
+
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
