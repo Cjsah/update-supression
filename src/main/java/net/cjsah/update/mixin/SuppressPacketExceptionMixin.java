c4b2985d766f9760f98690d7c7651d327cf5ae9d
Cjsah <cjsah@cjsah.com>
2024-04-02 23:12:00 +0800

    archiva

--- src/main/java/net/cjsah/update/mixin/SuppressPacketExceptionMixin.java
+++ src/main/java/net/cjsah/update/mixin/SuppressPacketExceptionMixin.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-@Mixin(NetworkThreadUtils.class)
+@Mixin(value = NetworkThreadUtils.class, remap = false)
 public class SuppressPacketExceptionMixin {
 
     @Shadow
